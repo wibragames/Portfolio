6009c4e5776e2edeebc8b77934e2457a72dc227c
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-12-15 15:38:51 +0100

    contact fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@mui/material/Container';
+import { Box, Button, Grid, TextField } from '@mui/material';
 
 export default function Contact(props: any) {
   const content = {
